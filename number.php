e397ff37b4fb761d9c5ebcf688406bc881c9f2e3
seonhyeokjun <tjsrmatn@naver.com>
2021-12-22 22:11:42 +0900

    Initial commit

---
+++ number.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number & Operator</title>
+</head>
+<body>
+    <h1>Number & Operator</h1>
+    <h2>1+1</h2>
+    <?php
+        echo 1+1;
+    ?>
+    <h2>2-1</h2>
+    <?php
+        echo 2-1;
+    ?>
+    <h2>2*2</h2>
+    <?php
+        echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?php
+        echo 4/2;
+    ?>
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String & String Operator</title>
+</head>
+<body>
+    <h1>String & String Operator</h1>
+    <?php
+        echo "Hello \"w\"orld";
+    ?>
+    <h2>concatenation operator</h2>
+    <?php
+        echo "Hello "."World";
+    ?>
+    <h2>String length function</h2>
+    <?php
+        echo strlen("Hello World");
+    ?>  
+</body>
+</html>
